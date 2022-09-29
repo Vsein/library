6d91320c6340855dc51331bc32a8a461624e55a2
Vsein <vseinssj@gmail.com>
2022-09-29 17:02:28 +0300

    Add the ability to add a new book

---
+++ logic.js
@@ -0,0 +1,79 @@
+let myLibrary = [];
+
+function Book(
+  title = "Unknown",
+  author = "Unknown",
+  pages = 0,
+  read = "Unread"
+) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+const overlay = document.getElementById('overlay');
+const addBookBtn = document.getElementById('addBookBtn');
+const addBookForm = document.getElementById('addBookForm');
+const addBookModal = document.getElementById('addBookModal');
+const submitBookBtn = document.getElementById('submitBookBtn');
+
+const openAddBookModal = () => {
+  console.log(1);
+  addBookForm.reset();
+  overlay.classList.add('active');
+  addBookModal.classList.add('active');
+}
+
+const closeAddBookModal = () => {
+  overlay.classList.remove('active');
+  addBookModal.classList.remove('active');
+}
+
+const getBookFromInput = () => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const stat = document.querySelector('input[name="status"]:checked').value;
+  console.log(1);
+  return new Book(title, author, pages, stat);
+}
+
+const addBookToLibrary = (e) => {
+  e.preventDefault();
+  let book = getBookFromInput();
+  console.log(1);
+  myLibrary.push(book);
+  addBookCard(book);
+  closeAddBookModal();
+}
+
+const addBookCard = (book) => {
+  const bookGrid = document.querySelector(".bookGrid");
+  const card = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+  card.classList.add('card');
+  h2.classList.add('title');
+  h2.textContent = book.title;
+  p.textContent = `Author: ${book.author}\r\nPages: ${book.pages}\r\n`;
+  if (book.read) {
+    p.textContent += `Status: Already read`;
+  } else {
+    p.textContent += `Status: In waiting list`;
+  }
+  card.appendChild(h2);
+  card.appendChild(p);
+  bookGrid.appendChild(card);
+}
+
+const displayBooks = () => {
+  for (let book of myLibrary) {
+    addBookCard(book);
+  }
+}
+
+addBookBtn.onclick = openAddBookModal;
+addBookForm.onsubmit = addBookToLibrary;
+
+displayBooks();
